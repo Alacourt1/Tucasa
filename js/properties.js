a9eb9ca654e9aafe155c48610cce263c2c6b8ee9
Alacourt1 <agustinmitrano@gmail.com>
2025-11-16 21:40:42 -0300

    propiedades.js
    Initial implementation of property management functions.

---
+++ js/properties.js
@@ -0,0 +1,35 @@
+// Gestión de propiedades inmobiliarias
+let properties = JSON.parse(localStorage.getItem('tucasa_properties')) || [];
+let propertyViews = JSON.parse(localStorage.getItem('tucasa_property_views')) || {};
+
+function loadAllProperties() {
+    // Cargar y mostrar todas las propiedades
+}
+
+function displayProperties(propertiesToShow) {
+    // Mostrar propiedades en la interfaz
+}
+
+function filterPropertiesByLocation(location) {
+    // Filtrar propiedades por ubicación
+}
+
+function setupPropertyModal() {
+    // Configurar modal de propiedades
+}
+
+function saveProperty() {
+    // Guardar propiedad (nueva o editada)
+}
+
+function editProperty(propertyId) {
+    // Editar propiedad existente
+}
+
+function deleteProperty(propertyId) {
+    // Eliminar propiedad
+}
+
+function registerPropertyView(propertyId) {
+    // Registrar vista de propiedad
+}
