fc11b281c4688ce53167d078a3a4624e93810349
Alacourt1 <agustinmitrano@gmail.com>
2025-11-16 21:43:07 -0300

    perfiles.js
    Added functions for user profile management.

---
+++ js/profile.js
@@ -0,0 +1,12 @@
+// Gestión de perfiles de usuario
+function showUserProfile(userId) {
+    // Mostrar perfil de usuario
+}
+
+function showCurrentUserProfile() {
+    // Mostrar perfil del usuario actual
+}
+
+function setupProfileEvents() {
+    // Configurar eventos del perfil
+}
